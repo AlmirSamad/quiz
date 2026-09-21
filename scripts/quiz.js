503b0ea878be10a83a5de106a83cd407cbcc05fb
Almir <almirsamad@gmail.com>
2026-09-21 09:41:47 -0400

    Aula 1

---
+++ scripts/quiz.js
@@ -0,0 +1,9 @@
+console.log('Quiz iniciando');
+
+const startQuiz = document.getElementById('start-quiz');
+
+console.log('texto do botão', startQuiz.textContent);
+
+startQuiz.textContent = 'Alterando o nome';
+
+console.log('texto do botão', startQuiz.textContent);
