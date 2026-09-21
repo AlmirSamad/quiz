1362e2af29c68b6d4cc234f0455a2eac71a215a0
Almir <almirsamad@gmail.com>
2026-09-21 15:03:54 -0400

    Ajustando o quiz

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,4 +1,88 @@
-const question1 = document.getElementById('question-1');
+const startQuizButton = document.querySelector('#start-quiz');
+const finishQuizButton = document.querySelector('#finish-quiz');
 
-console.log('Questão 1 ', question1);
-console.log('Questão 1 classe css:', question1.classList);
+const question1 = document.querySelector('#question-1');
+const question2 = document.querySelector('#question-2');
+const question3 = document.querySelector('#question-3');
+
+const scoreBoard = document.querySelector('#score-board');
+const score = document.querySelector('#score');
+let totalScore = 0;
+
+startQuizButton.addEventListener('click', () => {
+  console.log('Quiz iniciado');
+  startQuizButton.classList.add('quiz__btn--hidden');
+  question1.classList.remove('quiz__question--hidden');
+});
+
+question1.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const selectedAnswer = event.submitter;
+  const answer = selectedAnswer.textContent.trim();
+  const isCorrect = answer === 'Brasília';
+
+  selectedAnswer.classList.add(
+    isCorrect ? 'quiz__button--correct' : 'quiz__button--incorrect',
+  );
+
+  question1.querySelectorAll('.quiz__button--answer').forEach((button) => {
+    button.disabled = true;
+  });
+
+  if (isCorrect == true) {
+    totalScore++;
+    console.log(totalScore);
+  }
+
+  question2.classList.remove('quiz__question--hidden');
+});
+
+question2.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const selectedAnswer = event.submitter;
+  const answer = selectedAnswer.textContent.trim();
+  const isCorrect = answer === 'São Paulo';
+
+  selectedAnswer.classList.add(
+    isCorrect ? 'quiz__button--correct' : 'quiz__button--incorrect',
+  );
+
+  question1.querySelectorAll('.quiz__button--answer').forEach((button) => {
+    button.disabled = true;
+  });
+
+  if (isCorrect == true) {
+    totalScore++;
+    console.log(totalScore);
+  }
+
+  question3.classList.remove('quiz__question--hidden');
+});
+
+question3.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const selectedAnswer = event.submitter;
+  const answer = selectedAnswer.textContent.trim();
+  const isCorrect = answer === 'Amazonas';
+
+  selectedAnswer.classList.add(
+    isCorrect ? 'quiz__button--correct' : 'quiz__button--incorrect',
+  );
+
+  question1.querySelectorAll('.quiz__button--answer').forEach((button) => {
+    button.disabled = true;
+  });
+
+  if (isCorrect == true) {
+    totalScore++;
+    console.log(totalScore);
+  }
+
+  finishQuizButton.classList.remove('quiz__btn--hidden');
+  scoreBoard.classList.remove('quiz__score-card--hidden');
+});
+
+finishQuizButton.addEventListener('click', () => {
+  console.log('Quiz finalizado');
+});
